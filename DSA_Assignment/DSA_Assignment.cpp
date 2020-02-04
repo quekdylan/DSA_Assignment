a1d7da55030fb9861e4f320b19390f865ff218c7
Zhi En Dylan Quek <s10186935@connect.np.edu.sg>
2020-02-04 18:32:38 +0800

    Add project files.

---
+++ DSA_Assignment/DSA_Assignment.cpp
@@ -0,0 +1,102 @@
+// DSA_Assignment.cpp 
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+using namespace std;
+#include "Dictionary.h"
+
+//Methods
+void displayMenu();
+
+
+int main() {
+	//Import 
+	Dictionary stationCodes;
+	Dictionary stationNames;
+	List stationList;
+	ifstream infile("Stations.csv");
+	string line;
+	string station_code, station;
+	while (getline(infile, line)){
+		char delimiter = ',';
+		for (int i = 0; i < 2; i++){
+			size_t pos = line.find(delimiter);
+			string token = line.substr(0, pos);
+			if(i == 0){
+				station_code = token;
+			}
+			if(i ==1){
+				station = token;
+				stationNames.add(station, station_code);
+				stationCodes.add(station_code, station);
+			}
+			line.erase(0, pos + 1);
+		}
+	}
+
+	int choice = 1;
+	while (true) {
+		displayMenu();
+		cin >> choice;
+
+		if (choice == 1) {
+			stationCodes.print();
+			continue;
+		}
+		// Display station information for a given station name
+		if (choice == 2) {							
+			cout << "Please enter station name: ";
+			string input;
+			cin >> input; 
+			cout << stationNames.get(input);
+			continue;
+		}
+
+		if (choice == 3) {
+			cout << "Please enter station name that you would like to add" << endl;
+			string name;
+			cin >> name;
+			//string
+			//stations.add(input);
+
+		}
+
+		// Find and display a route and its price, given the source and destination stations
+		if (choice == 4) {
+			cout << "Please enter source station name" << endl;
+			string source;
+			cin >> source;
+			cout << "Please enter destination station name" << endl;
+			string dest;
+			cin >> dest;
+
+		}
+
+		if (choice == 0) {
+			break;
+		}
+		else {
+			cout << "Invalid option. Please try again." << endl;
+		}
+	}
+
+	//chat
+	//
+
+}
+
+void displayMenu()
+{
+	cout << endl;
+	cout << "MRT Hub \n";
+	cout << "--------------------------------\n";
+	cout << "[1] Display stations\n";
+	cout << "[2] Search station \n";
+	cout << "[3] Add station \n";
+	cout << "[4] Find Route \n";
+	cout << "[0] Exit \n";
+	cout << "--------------------------------\n";
+	cout << "Enter option : ";
+}
---
+++ DSA_Assignment/Dictionary.cpp
@@ -0,0 +1,158 @@
+#include<string>
+#include<iostream>
+using namespace std;
+
+#include "Dictionary.h"
+
+Dictionary::Dictionary() {
+	for (int i = 0; i < MAX_SIZE; i++) {
+		items[i] = NULL;
+	}
+	size = 0;
+}
+
+Dictionary::~Dictionary() {}
+
+int charvalue(char c)
+{
+	if (isalpha(c))
+	{
+		if (isupper(c))
+			return (int)c - (int)'A';
+		else
+			return (int)c - (int)'a' + 26;
+	}
+	else
+		return -1;
+}
+
+
+int Dictionary::hash(KeyType key)
+{
+	int keyValue = 0;
+
+	for (char& c : key)
+	{
+		keyValue += charvalue(c);
+	}
+
+	return(keyValue % MAX_SIZE);
+}
+
+bool Dictionary::add(KeyType newKey, ItemType newItem)
+{
+	if (items[hash(newKey)] == NULL)
+	{
+		Node* newNode = new Node;
+		//Create a new Node
+		items[hash(newKey)] = newNode;
+		//Store newItem in new Node
+		newNode->item = newItem;
+		newNode->key = newKey;
+		newNode->next = NULL;
+	}
+
+	else {
+
+		Node* tempNode = items[hash(newKey)];
+		if (tempNode->key == newKey)
+		{
+			return false;
+		}
+
+		while (tempNode->next != NULL)
+		{
+			tempNode = tempNode->next;
+			if (tempNode != NULL && tempNode->key == newKey)
+			{
+				return false;
+			}
+		}
+
+		Node* newNode = new Node;
+		newNode->item = newItem;
+		newNode->key = newKey;
+		newNode->next = NULL;
+		tempNode->next = newNode;
+	}
+	size++;
+	return true;
+}
+
+void Dictionary::remove(KeyType key)
+{
+	Node* newNode = items[hash(key)];
+	Node* prevNode = items[hash(key)];
+	if (items[hash(key)] != NULL)
+	{
+		if (newNode->key == key)
+		{
+			items[hash(key)] = NULL;
+		}
+
+		while (newNode != NULL)
+		{
+			if (newNode->key == key)
+			{
+				newNode = NULL;
+				size--;
+				break;
+			}
+			newNode = newNode->next;
+		}
+
+	}
+}
+
+ItemType Dictionary::get(KeyType key)
+{
+	Node* tempNode = items[hash(key)];
+	while (tempNode != NULL)
+	{
+		if (tempNode->key == key)
+		{
+			return tempNode->item;
+		}
+		tempNode = tempNode->next;
+
+	}
+}
+
+bool Dictionary::isEmpty()
+{
+	if (size == 0)
+	{
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+int Dictionary::getLength() {
+	return size;
+}
+
+void Dictionary::print()
+{
+	Node* tempNode = new Node;
+	for (int i = 0; i < MAX_SIZE; i++)
+	{
+
+		if (items[i] != NULL)
+		{
+
+			tempNode = items[i];
+			while (tempNode != NULL)
+			{
+				cout << tempNode->item << "        " << tempNode->key << endl;
+				tempNode = tempNode->next;
+
+			}
+		}
+	}
+}
+
+
+
+
